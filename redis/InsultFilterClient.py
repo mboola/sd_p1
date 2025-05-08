f9c49c49b820a945a54fbce6edaf79fd234ee349
Marcel Povill <mpovilleti@gmail.com>
2025-05-08 19:19:22 +0200

    Fixed tests in reddis

--- redis/InsultFilterClient.py
+++ redis/InsultFilterClient.py
@@ -1,4 +1,7 @@
 import redis
+import sys
+
+petitions = int(sys.argv[1])
 
 # Connect to Redis
 server = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
@@ -6,6 +9,6 @@ server = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
 texts_queue = "texts_queue"
 raw_texts = ["Eres un papanatas!", "Tremendo bobo", "Como puedes ser tan estupido, estupido?", "bobete bobete bobo bobete"]
 
-for i in range(100):
+for i in range(petitions):
     print(f"Adding text!")
     server.rpush(texts_queue, raw_texts[i % 4])
--- redis/metrics_tester.sh
+++ redis/metrics_tester.sh
@@ -1,33 +1,37 @@
 #! /bin/bash
 
-sudo apt update
-sudo apt install apparmor apparmor-utils
+# TODO: Check if python has virtual env
+#   -> milax doesn't enable pip install redis
+python3 -m venv venv
+source venv/bin/activate
+pip install redis
 
 if [ -f time_log ]; then
     rm time_log
 fi
 
-./system_initializer.sh 1 100
-./system_initializer.sh 2 100
-./system_initializer.sh 3 100
-./system_initializer.sh 4 100
+
+#./system_initializer.sh 1 100
+#./system_initializer.sh 2 100
+#./system_initializer.sh 3 100
+#./system_initializer.sh 4 100
 
 ./system_initializer.sh 1 200
-./system_initializer.sh 2 200
-./system_initializer.sh 3 200
-./system_initializer.sh 4 200
+#./system_initializer.sh 2 200
+#./system_initializer.sh 3 200
+#./system_initializer.sh 4 200
 
-./system_initializer.sh 1 500
-./system_initializer.sh 2 500
-./system_initializer.sh 3 500
-./system_initializer.sh 4 500
+#./system_initializer.sh 1 500
+#./system_initializer.sh 2 500
+#./system_initializer.sh 3 500
+#./system_initializer.sh 4 500
 
-./system_initializer.sh 1 1000
-./system_initializer.sh 2 1000
-./system_initializer.sh 3 1000
-./system_initializer.sh 4 1000
+#./system_initializer.sh 1 1000
+#./system_initializer.sh 2 1000
+#./system_initializer.sh 3 1000
+#./system_initializer.sh 4 1000
 
-./system_initializer.sh 1 5000
-./system_initializer.sh 2 5000
-./system_initializer.sh 3 5000
-./system_initializer.sh 4 5000
+#./system_initializer.sh 1 5000
+#./system_initializer.sh 2 5000
+#./system_initializer.sh 3 5000
+#./system_initializer.sh 4 5000
