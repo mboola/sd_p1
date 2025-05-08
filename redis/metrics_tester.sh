f2895d944a056a77c30b0896de221190aa38b6b5
Marcel Povill <mpovilleti@gmail.com>
2025-05-08 19:22:01 +0200

    branch for merge

--- redis/metrics_tester.sh
+++ redis/metrics_tester.sh
@@ -14,12 +14,12 @@ fi
 #./system_initializer.sh 1 100
 #./system_initializer.sh 2 100
 #./system_initializer.sh 3 100
-#./system_initializer.sh 4 100
+./system_initializer.sh 4 100
 
-./system_initializer.sh 1 200
+#./system_initializer.sh 1 200
 #./system_initializer.sh 2 200
 #./system_initializer.sh 3 200
-#./system_initializer.sh 4 200
+./system_initializer.sh 4 200
 
 #./system_initializer.sh 1 500
 #./system_initializer.sh 2 500
@@ -27,11 +27,11 @@ fi
 #./system_initializer.sh 4 500
 
 #./system_initializer.sh 1 1000
-#./system_initializer.sh 2 1000
+./system_initializer.sh 2 1000
 #./system_initializer.sh 3 1000
 #./system_initializer.sh 4 1000
 
 #./system_initializer.sh 1 5000
 #./system_initializer.sh 2 5000
-#./system_initializer.sh 3 5000
+./system_initializer.sh 3 5000
 #./system_initializer.sh 4 5000
