5e1909a395e8d4bdbe49ce4321ee3534498f8f6a
Marcel Povill <mpovilleti@gmail.com>
2025-05-14 01:30:05 +0200

    Added xmlrpc results

--- xmlrpc/metrics_tester.sh
+++ xmlrpc/metrics_tester.sh
@@ -1,32 +1,34 @@
 #! /bin/bash
 
-if [ -f time_log ]; then
-    rm time_log
+if [ -f logging/InsultFilterService.log ]; then
+    rm logging/InsultFilterService.log
 fi
 
-./system_initializer.sh 1 2000 4   
+if [ -f logging/InsultService.log ]; then
+    rm logging/InsultService.log
+fi
 
-#./system_initializer.sh 1 100 4
-#./system_initializer.sh 2 100 4
-#./system_initializer.sh 3 100 4
-#./system_initializer.sh 4 100 4
+./system_initializer.sh 1 500 4
+./system_initializer.sh 2 500 4
+./system_initializer.sh 3 500 4
+./system_initializer.sh 4 500 4
 
-#./system_initializer.sh 1 200 4
-#./system_initializer.sh 2 200 4
-#./system_initializer.sh 3 200 4
-#./system_initializer.sh 4 200 4
+./system_initializer.sh 1 1000 4
+./system_initializer.sh 2 1000 4
+./system_initializer.sh 3 1000 4
+./system_initializer.sh 4 1000 4
 
-#./system_initializer.sh 1 500 4
-#./system_initializer.sh 2 500 4
-#./system_initializer.sh 3 500 4
-#./system_initializer.sh 4 500 4
+./system_initializer.sh 1 5000 4
+./system_initializer.sh 2 5000 4
+./system_initializer.sh 3 5000 4
+./system_initializer.sh 4 5000 4
 
-#./system_initializer.sh 1 1000 4
-#./system_initializer.sh 2 1000 4
-#./system_initializer.sh 3 1000 4
-#./system_initializer.sh 4 1000 4
+./system_initializer.sh 1 10000 4
+./system_initializer.sh 2 10000 4
+./system_initializer.sh 3 10000 4
+./system_initializer.sh 4 10000 4
 
-#./system_initializer.sh 1 5000 4
-#./system_initializer.sh 2 5000 4
-#./system_initializer.sh 3 5000 4
-#./system_initializer.sh 4 5000 4
+./system_initializer.sh 1 20000 4
+./system_initializer.sh 2 20000 4
+./system_initializer.sh 3 20000 4
+./system_initializer.sh 4 20000 4
